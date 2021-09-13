52280aae24c6de0181cfab63ca2286492e937689
lucas diekvoss <7276702@isd535.org>
2021-09-13 09:29:52 -0500

    added ball and block

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 // Put your JavaScript here
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillstyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillstyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
+
