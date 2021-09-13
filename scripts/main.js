927fea3249fa91a280fabc05f3dc0e9d9237787a
lucas diekvoss <7276702@isd535.org>
2021-09-13 09:46:08 -0500

    added ball movement

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,42 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+let dx = 2;
+let dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    //clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //draw the ball
+    drawBall();
+
+    //let the x and y values change for the ball
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10)
+
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
-ctx.fillstyle = "#FF0000";
+ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillstyle = "green";
+ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
 
