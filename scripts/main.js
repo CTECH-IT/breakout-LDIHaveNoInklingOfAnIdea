22f0dfdcff7c1fa861ae51e824974e092658e81a
lucas diekvoss <7276702@isd535.org>
2021-09-14 09:19:12 -0500

    ball can bounce

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,11 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -27,6 +29,15 @@ function draw() {
     //let the x and y values change for the ball
     x += dx;
     y += dy;
+
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    if (x + dx > canvas.width - ballRadius || x + dx  < ballRadius) {
+        dx = -dx;
+    }
+    
 }
 setInterval(draw, 10)
 
